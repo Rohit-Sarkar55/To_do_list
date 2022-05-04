18e5c4eda6ee0622fefa2c562f1da9503a49a9d0
Rohit-Sarkar55 <71070509+Rohit-Sarkar55@users.noreply.github.com>
2022-05-04 18:56:17 +0530

    Some changes

--- My_To_Do_List/index.js
+++ My_To_Do_List/index.js
@@ -15,12 +15,28 @@ app.get("/" ,function(req ,res){
             break;
         case 1:
             day = "Monday"
-        default:
+        case 2:
             day = "Tuesday"
             break;
-    }
+        case 3:
+            day = "Wednesday"
+            break;
+        case 4:
+            day = "Thursday"
+            break;
+        case 5:
+            day = "Friday"
+            break;
+        case 6:
+            day = "Saturday"
+            break;
 
-    
+        default:
+            console.log("Error Current Day is " + Current_Day );
+        }
+
+    res.render("list" , {NameOfDay:day});
+    /*
     if(Current_Day === 6 || Current_Day === 0){
       // day ="Weekend";
         res.render("list", {KindOfDay:day});
@@ -29,7 +45,7 @@ app.get("/" ,function(req ,res){
        //day = "WeekDay";
        res.render("list", {KindOfDay:day});
     }
-
+    */
 });
 
 app.listen(3000 ,function(){
